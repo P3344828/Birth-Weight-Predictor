b9b83c17d3bef25fda12062061d3ee08054e6436
P3344828 <priyanka971794@gmail.com>
2026-05-03 16:06:10 +0530

    "Cleaned code, fixed model path bug and improved UI integration"

--- app.py
+++ app.py
@@ -1,54 +1,25 @@
-from flask import Flask, request, render_template
-import pickle
-import pandas as pd
+from flask import Flask, render_template
+from routes import predict, user
+from extentions import cache
 
 app = Flask(__name__)
 
-def get_cleaned_data(form_data):
-    gestation = float(form_data['gestation'])
-    parity = int(form_data['parity'])
-    age = float(form_data['age'])
-    height = float(form_data['height'])
-    weight = float(form_data['weight'])
-    smoke = float(form_data['smoke'])
+## configure my cache
+app.config['CACHE_TYPE']= 'SimpleCache'
 
-    cleaned_data = {
-        'gestation': [gestation],
-        'parity': [parity],
-        'age': [age],
-        'height': [height],
-        'weight': [weight],
-        'smoke': [smoke]
-    }
+## init (intialize) cache
+cache.init_app(app)
 
-    return cleaned_data
+
+## import your blueprints here and register
+app.register_blueprint(user.user_bp)
+app.register_blueprint(predict.predict_bp)
+
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
 
-## define your endpoint
-
-@app.route('/predict', methods=['POST'])
-def get_prediction():
-    # Get data from user
-    baby_data_form = request.form
-
-    baby_data_cleaned = get_cleaned_data(baby_data_form)
-
-    ## convert into dataframe
-    baby_df = pd.DataFrame(baby_data_cleaned)
-
-    # Load the ML model
-    with open('model.pkl', 'rb') as obj:
-        model = pickle.load(obj)
-
-    # Make prediction on user data
-    prediction = model.predict(baby_df)
-    prediction = round(float(prediction), 2)
-
-    # Render result back to HTML
-    return render_template('index.html', prediction=prediction)
-
 if __name__ == '__main__':
     app.run(debug=True)
+
